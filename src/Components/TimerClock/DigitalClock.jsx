271f6d2438ff5bfe3a28fc9f130d993b182df709
AbdulMoiz8994 <moiza8994@gmail.com>
2021-07-06 10:10:06 +0500

    add the setInterval Event after one second it will call the function  and run it

--- src/Components/TimerClock/DigitalClock.jsx
+++ src/Components/TimerClock/DigitalClock.jsx
@@ -1,13 +1,19 @@
-import React from 'react';
+import React,{useState} from 'react';
 
 
 export const DigitalClock=() =>{
 
     const time=new Date().toLocaleTimeString()
+   const [cTime, setTime]=useState()
+     const updateDate=() =>{
+         setTime(time)
+     }
+    setInterval(updateDate,1000) 
    return(
        <div>
         <h1>Digital Clock</h1>           
          <h1>{time}</h1>
+         {/* <button onClick={updateDate}>Click</button> */}
        </div>
    )
 }
